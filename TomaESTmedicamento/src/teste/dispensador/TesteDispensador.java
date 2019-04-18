6ffe67989d2ff42919c3e1fca5bbfa683b35e669
Tó <T@LNV-LGN-Y520>
2019-04-18 21:06:40 +0100

    Alteraçoes no Dispensador e criei a classe TesteDispensador

---
+++ TomaESTmedicamento/src/teste/dispensador/TesteDispensador.java
@@ -0,0 +1,20 @@
+package teste.dispensador;
+
+import tomaest.maquina.Dispensador;
+
+public class TesteDispensador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Dispensador d1 =new Dispensador ("InstaPoo", 0,1);
+	//	d1.setQuantidadeAtual(0);
+	//	d1.addQuantidade(10);
+	//	d1.despeja(5);
+		
+		System.out.println(d1.getNomeMedicamento());
+		
+
+	}
+
+}
--- TomaESTmedicamento/src/tomaest/maquina/Dispensador.java
+++ TomaESTmedicamento/src/tomaest/maquina/Dispensador.java
@@ -7,27 +7,102 @@ import java.util.List;
  * Cada dispensador tem um medicamento e vários comprimidos (ou cápsulas)
  * desse medicamento
  */
+
+	
+
+
 public class Dispensador {
 
+	
+	private String nomeMedicamento;
+	private int posiçaoDisp;
+	private int quantidadeAtual;
+	
+	
+	
 	/** adiciona numa certa quantidade de comprimidos ao dispensador
-	 * @param quant quantidade a adicionar ao dispensador
 	 */
-	public void addQuantidade(int quant) {
+	
+	
+	
+	
+	
+	
+	public Dispensador(String nomeMedicamento, int quantidadeAtual, int posiçaoDisp) {
+	
+		setQuantidadeAtual(quantidadeAtual);
+		setNomeMedicamento(nomeMedicamento);
+		setPosiçaoDisp (posiçaoDisp);
+	}
+
+	
+
+	public String getNomeMedicamento() {
+		return nomeMedicamento;
+	}
+
+	public void setNomeMedicamento(String nomeMedicamento) {
+		this.nomeMedicamento = nomeMedicamento;
+	}
+
+
+	
+
+	public int getPosiçaoDisp() {
+		return posiçaoDisp;
+	}
+
+	public void setPosiçaoDisp(int posiçaoDisp) {
+		this.posiçaoDisp = posiçaoDisp;
 	}
 	
+
+
+
+	public int getQuantidadeAtual() {
+		return quantidadeAtual;
+	}
+	
+	public void setQuantidadeAtual(int quantidadeAtual) {
+		this.quantidadeAtual = quantidadeAtual;
+	}
+
+
+
+
+
+	public int addQuantidade(int quant) {
+		
+		return quantidadeAtual=quantidadeAtual+quant;
+		
+		
+		
+	}
+
 	/** despeja, na gaveta de saída, a quantidade solicitada de comprimidos.
 	 * @param quant quantidade de comprimidos a despejar
 	 * @return a quantidade realmente despejada, pois pode não haver quantidade
 	 *  suficiente para satisfazer o pedido
 	 */
 	public int despeja( int quant ) {
-		return 0;
+		if(quant<=quantidadeAtual) {
+			quantidadeAtual=quantidadeAtual-quant;
+		return quantidadeAtual;
+		}
+		else
+		
+		return 0;  //Devia avisar que não tem stock que chegue mas nao sei como...
+		
 	}
 	
 	/** indica se este dispensador está a ser usado ou se está livre
 	 * @return true, se está a ser usado, false caso esteja livre
 	 */
 	public boolean estaUsado( ) {
+		if(quantidadeAtual>0) {
+			return true;			//Vai haver confusão entre estar a ser usado ou ter esgotado o stock!
+			
+		}else
 		return false;
 	}
 	
