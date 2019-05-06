5f11c7294ab2164eff8103ec805e3520b93370ab
tmsng <tmsng@192.168.1.70>
2019-05-06 12:20:42 +0100

    Verificar alteraçoes

--- TomaESTmedicamento/src/tomaest/maquina/TomaParcial.java
+++ TomaESTmedicamento/src/tomaest/maquina/TomaParcial.java
@@ -3,6 +3,7 @@ package tomaest.maquina;
 /** Classe que representa uma toma parcial, isto é, qual o dispensador
  * e número de comprimidos a usar desse dispensador. A hora a que os comprimidos
  * devem ser tomados é dada pela classe Toma 
+ * teste
  */
 public class TomaParcial {
 
